80254b62105c897b471e81d8dbc8ac1f8ca42ffa
Northsan <northssan@gmail.com>
2020-10-21 10:29:14 +0200

    MCU - Body layout added

--- src/app/layouts/layouts.module.ts
+++ src/app/layouts/layouts.module.ts
@@ -1,17 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
+import { BodyComponent } from './body/body.component';
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
    declarations: [
-      HeaderComponent
+      HeaderComponent,
+      BodyComponent
    ],
    imports: [
-      CommonModule
+      CommonModule,
+      RouterModule
    ],
    exports: [
-      HeaderComponent
+      HeaderComponent,
+      BodyComponent
    ]
 })
 export class LayoutsModule { }
