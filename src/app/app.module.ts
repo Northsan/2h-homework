a712613ea101f4d2801dd0687de559ec3f2c9ce4
Northsan <northssan@gmail.com>
2020-10-21 10:23:30 +0200

    MCU - App-Routing added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BackendService } from './backend.service';
 
 @NgModule({
-    declarations: [AppComponent],
-    imports: [BrowserModule],
-    providers: [BackendService],
-    bootstrap: [AppComponent]
+   declarations: [
+      AppComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule
+   ],
+   providers: [
+      BackendService
+   ],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule {
 
